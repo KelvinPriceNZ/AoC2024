642b1d3d4add8b4aa891a294eaa0a6a9580274a0
Kelvin <anonymous@nowhere.geek.nz>
2024-11-30 15:01:59 +1300

    add .gitignore and some helper tools

---
+++ mkday.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+BASEDIR=$(dirname $0)
+cd $BASEDIR
+
+if [[ -z $1 ]]
+then
+   echo "No day number provided"
+   exit 1
+fi
+
+printf -v day "%02d" $1
+
+[ ! -d $day ] && mkdir -p $day
+
+#cp -p solve.py $day/part1.py
+#cp -p solve.py $day/part2.py
+
+#./render_md.py $day
+
+FILE="input/${day}/input.txt"
+[ ! -d $(dirname $FILE) ] && mkdir -p $(dirname $FILE)
+
+if [ ! -s ./${FILE} ]
+then
+   echo "Fetching $FILE"
+   wget --no-cookies --header "Cookie: session=$(<./.token)" https://adventofcode.com/2024/day/$1/input -O ${FILE}
+else
+   echo "$FILE already exists with data"
+fi
+
+head ${FILE}
+tail ${FILE}
