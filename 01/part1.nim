d7dc9002ee58248be4626e592b415cfae5b497d7
Kelvin <anonymous@nowhere.geek.nz>
2024-12-01 19:27:35 +1300

    Day 1 use splitWhitespace instead of split

--- 01/part1.nim
+++ 01/part1.nim
@@ -8,9 +8,9 @@ var
   left, right: seq[int]
 
 for l in "../input/01/input.txt".lines:
-  let words = l.split(' ')
+  let words = l.splitWhitespace
   var num1 = words[0].parseInt
-  var num2 = words[^1].parseInt
+  var num2 = words[1].parseInt
   left.add(num1)
   right.add(num2)
 
