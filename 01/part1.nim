4b0cfa537b3750f3bf2cac584ea5d12d3baaa1fd
Kelvin <anonymous@nowhere.geek.nz>
2024-12-01 18:57:55 +1300

    Day 1 in nim !

---
+++ 01/part1.nim
@@ -0,0 +1,23 @@
+import strutils
+import std/strformat
+import std/algorithm
+
+var sum: int = 0
+
+var
+  left, right: seq[int]
+
+for l in "../input/01/input.txt".lines:
+  let words = l.split(' ')
+  var num1 = words[0].parseInt
+  var num2 = words[3].parseInt
+  left.add(num1)
+  right.add(num2)
+
+left.sort
+right.sort
+
+for n in 0..<left.len:
+  sum += abs(left[n] - right[n])
+
+echo &"{sum}"
