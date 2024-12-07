394f852d51ad0def175a3cd5e2cebf9bd7708faa
Kelvin <anonymous@nowhere.geek.nz>
2024-12-08 11:35:41 +1300

    Day 7 initisl solutions

---
+++ 07/Puzzle.md
@@ -0,0 +1,5 @@
+# Advent of Code 2023
+
+## Day 7
+
+[2024 - Day 7](https://adventofcode.com/2024/day/7 "Advent of Code 2024 Day 7")
---
+++ 07/part2.nim
@@ -0,0 +1,50 @@
+import strutils
+import std/strformat
+import sequtils
+
+var
+  sum: int = 0
+
+
+proc calc(soln: int, answer: var int, terms: seq[int], candidates: var seq[int], depth: int): seq[int] =
+  if depth == terms.high:
+    if answer == soln:
+      candidates.add(answer)
+    return candidates
+  else:
+    for op in ['+','*','&']:
+      var curr: int = answer
+      case op:
+        of '+':
+          answer += terms[depth + 1]
+        of '*':
+          answer *= terms[depth + 1]
+        of '&':
+          answer = parseInt($answer & $terms[depth + 1])
+        else:
+          discard
+
+      if answer <= soln:
+        discard calc(soln, answer, terms, candidates, depth + 1)
+      else:
+        discard calc(soln, answer, terms, candidates, terms.high)
+
+      answer = curr
+
+
+for l in "../input/07/input.txt".lines:
+  var termlist: string
+  let words = l.split(':')
+  var solution: int = words[0].parseInt
+  termlist = words[1]
+  var terms: seq[int] = termlist.splitWhitespace.toSeq.mapIt(parseInt(it))
+
+  var choices: seq[int]
+
+  discard calc(solution, terms[0], terms, choices, 0)
+  for ans in choices:
+    if ans == solution:
+      sum += solution
+      break
+
+echo &"{sum}"
